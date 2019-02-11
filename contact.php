178d5059923a1c1052a6a8749d3802fac91b42f2
Hammed01 <shotolahammed01@gmail.com>
2019-02-11 14:44:03 +0100

    Redesign of the whole website

---
+++ contact.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>I'm  Oluwole, a Product Designer</title>
+    </head>
+    <body>
+            <div class="container-flex">
+                    
+                <nav class="sticky"> 
+                    <div class="row">
+                            <a class="logo"><img src="logo1.png"></a>
+                     
+                      <ul class="main-nav">                         
+                            <li><a href="index.html">WORK</a></li>
+                            <li><a   href="about.html ">ABOUT</a></li>
+                            <li><a class="nav-button" href="contact.html">CONTACT</a></li>
+                        </ul>
+                        <a class="mobile-nav-icon js-nav-icon"><i class="fas fa-bars"></i></a>
+                    </div>
+                
+                </nav>
+                    
+                    </nav>
+            <div class="contact-container">
+                <h3>Need to start a project or you just want to say Hi <br> Kindly fill this form and i will 
+                    <br> get back to you.</h3>
+            
+                    <form name="contact" method="post" action="send_form_email.php">
+                        
+                        <label for="Name"> Name</label>
+                        <input type="text" name="first_name" size="35"><br>
+                    
+                    
+                        <label for="Email"> Email</label>
+                        <input type="text" name="email"  size="35"><br>
+                       
+                        <!-- <label for="Name"> Subject</label>
+                        <input type="text" name="Subject" size="35"><br> -->
+                        <label for="subject"> Message </label>
+                        <textarea rows="10" cols="54" name="comments" > </textarea> 
+                        <br>
+                        <label for="submit"> . </label>
+                       <input type="submit" value="Submit" size="35">
+                       
+                     </form>
+                    
+                </section>
+            </div>
+            </div>
+
+           
+
+                <footer>
+                        <ul>
+                                <a href="https://tinyurl.com/y74m9rcg"><li><i class="fab fa-linkedin-in"></i> </li></a>
+                                <a href="https://tinyurl.com/yafmpdeu"></a><li><i class="fab fa-twitter"></i></li></a>
+                                <a href="https://tinyurl.com/y7svlc9l"> <li><i class="fab fa-dribbble"></i></li></a>
+                                <a href="https://tinyurl.com/y7svlc9l"> <li><i class="fab fa-behance"></i></li> </a>
+                                
+                            </ul>
+        
+                    </footer>
+
+            
+        <!-- <script src="" async defer></script> -->
+    </body>
+</html>
